b010a305005e2a84a30f0af2962afbca8e4ff594
moad <charhbili.moad.etudes@gmail.com>
2020-02-11 19:32:01 +0100

    Update three players

---
+++ nash_GUI/nash_eq.py
@@ -0,0 +1,51 @@
+import sys, argparse, nash
+
+# Defining Parsing arguments :
+parser = argparse.ArgumentParser(description='Calculates pure and mixed Nash equilibrium strategies.')
+
+parser.add_argument("-f", "--file", dest="filename", help="defines the path to the file containing the utility function", metavar="FILE", required=True)
+parser.add_argument("-p","--pure", dest="pure", action='store_true', help="search for pure nash strategies")
+parser.add_argument("-m","--mixed", dest="mixed", action='store_true', help="search for mixed nash strategies")
+parser.add_argument("-plt","--plot", dest="plt",default=False, action='store_true', help="Plot the planes of the mixed strategies of each player and their intersections")
+parser.add_argument("-N","--number-of-players",nargs='?',dest="number_players", help="defines the number for playing players, default to 2 if not specified",  metavar="NumberOfPlayers", type=int)
+parser.add_argument("-l1","--label-p1", dest="label1", default=None, help="define a label for player 1", metavar="LABEL")
+parser.add_argument("-l2","--label-p2", dest="label2", default=None, help="define a label for player 2", metavar="LABEL")
+parser.add_argument("-l3","--label-p3", dest="label3", default=None, help="define a label for player 3", metavar="LABEL")
+parser.add_argument("-s1","--strategies-p1", nargs='*', dest="strategies1", default=None, help="define strategies' labels for player 1", metavar="LABEL")
+parser.add_argument("-s2","--strategies-p2", nargs='*', dest="strategies2", default=None, help="define strategies' labels for player 2", metavar="LABEL")
+parser.add_argument("-s3","--strategies-p3", nargs='*', dest="strategies3", default=None, help="define strategies' labels for player 3", metavar="LABEL")
+
+args = parser.parse_args()
+
+
+# Checking the arguments for specific requirements :
+# no strategy was specified 
+if not (args.pure or args.mixed) :  
+    parser.print_help(file=sys.stderr)
+    print("\n\nSpecify a strategy to use, available options : [-p, -m]", file=sys.stderr)
+    exit(1)
+# the number of players passed as argument is different than 2 and 3
+elif args.number_players!=None and 2 != args.number_players != 3 :
+    parser.print_help(file=sys.stderr)
+    print("\n\nSpecify the number of players, available options : [2, 3]", file=sys.stderr)
+    exit(2)
+# labels were specified for players which weren't specified
+elif args.number_players != 3 and args.label3 != None :
+    parser.print_help(file=sys.stderr)
+    print("\n\nA label was specified for a played which doesn't exist", file=sys.stderr)
+    exit(3)
+elif args.number_players != 3 and args.strategies3 != None :
+    parser.print_help(file=sys.stderr)
+    print("\n\nStrategy labels were specified for a played which doesn't exist", file=sys.stderr)
+    exit(4)
+
+# Preparing an instance from our game. 'Tableau' 
+nsh = nash.Nash(args.filename, 
+                number_of_players=args.number_players,
+                labels=[args.label1, args.label2, args.label3],
+                strategies=[args.strategies1, args.strategies2, args.strategies3])
+
+if args.pure:
+    nsh.compute_pure_strategies()
+if args.mixed:
+    nsh.compute_mixed_strategies(args.plt)
